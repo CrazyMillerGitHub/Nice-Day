c95abee97ede80d5b3560bb698bf49fc0f9a1526
Mikhail Borisov <mishaborisov2013@gmail.com>
2019-07-05 15:08:03 +0300

    -Рефакторинг

---
+++ Nice Day/MainViewController/Controller/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  Nice Day
+//
+//  Created by Михаил Борисов on 04/07/2019.
+//  Copyright © 2019 Mikhail Borisov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    let provider = MainViewControllerProvider()
+    let delegate = MainViewControllerDelegate()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = provider
+        collectionView.delegate = delegate
+        // Do any additional setup after loading the view.
+    }
+
+}
