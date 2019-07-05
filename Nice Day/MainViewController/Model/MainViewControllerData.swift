200e797007c1d54b56ae57a806fb01b796bfb72e
Mikhail Borisov <mishaborisov2013@gmail.com>
2019-07-05 18:04:14 +0300

    -Создал Singleton

---
+++ Nice Day/MainViewController/Model/MainViewControllerData.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewControllerData.swift
+//  Nice Day
+//
+//  Created by Михаил Борисов on 05/07/2019.
+//  Copyright © 2019 Mikhail Borisov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewControllerData {
+    
+    static let shared = MainViewControllerData()
+    private init() {}
+    var arr = Array(0..<4)
+}
