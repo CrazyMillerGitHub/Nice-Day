9076c5dc2d1585984b1efb5b3e87618367992504
Mikhail Borisov <mishaborisov2013@gmail.com>
2019-07-06 22:27:10 +0300

    -Added Cells

---
+++ Nice Day/MainViewController/Model/cells/FriendsCell.swift
@@ -0,0 +1,13 @@
+//
+//  FriendsCell.swift
+//  Nice Day
+//
+//  Created by Михаил Борисов on 06/07/2019.
+//  Copyright © 2019 Mikhail Borisov. All rights reserved.
+//
+
+import UIKit
+
+class FriendsCell: UICollectionViewCell {
+    
+}
