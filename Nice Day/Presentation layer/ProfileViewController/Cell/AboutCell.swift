5bd21d582654acedd81ffe515c984fe2752e095d
Mikhail Borisov <mishaborisov2013@gmail.com>
2019-10-20 21:17:19 +0300

    -added two cells

---
+++ Nice Day/Presentation layer/ProfileViewController/Cell/AboutCell.swift
@@ -0,0 +1,21 @@
+//
+//  AboutCell.swift
+//  Nice Day
+//
+//  Created by Михаил Борисов on 20.10.2019.
+//  Copyright © 2019 Mikhail Borisov. All rights reserved.
+//
+
+import UIKit
+
+class AboutCell: UICollectionViewCell {
+    static var identifier = "about"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Nice Day/Presentation layer/ProfileViewController/Cell/ProfileAchievmentsCell.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileAchievmentsCell.swift
+//  Nice Day
+//
+//  Created by Михаил Борисов on 20.10.2019.
+//  Copyright © 2019 Mikhail Borisov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileAchievmentsCell: UICollectionViewCell {
+    static var identifier = "profileAchievments"
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
