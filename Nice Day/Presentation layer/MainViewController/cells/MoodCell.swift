f50281c8ccb6d27d77fd623acf69c6dd433078ee
Mikhail Borisov <mishaborisov2013@gmail.com>
2019-11-09 19:21:34 +0300

    -added Close Button

--- Nice Day/Presentation layer/MainViewController/cells/MoodCell.swift
+++ Nice Day/Presentation layer/MainViewController/cells/MoodCell.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class MoodCell: CoreCell {
+    
     static var identifier: String = "mood"
     
     var item: MainViewModelItem? {
@@ -17,7 +18,37 @@ class MoodCell: CoreCell {
            }
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    let closeButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .sunriseColor
+        button.setImage(UIImage(named: "Home"), for: .normal)
+        button.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+        button.layer.cornerRadius = 15
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.shadowColor = UIColor.sunriseColor.cgColor
+        button.layer.shadowRadius = 16.0
+        button.layer.shadowOpacity = 0.14
+        button.layer.shadowOffset = CGSize(width: 0, height: 16)
+        button.layer.masksToBounds = false
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        contentView.addSubview(closeButton)
+        NSLayoutConstraint.activate([
+            closeButton.widthAnchor.constraint(equalToConstant: 46),
+            closeButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            closeButton.heightAnchor.constraint(equalTo: contentView.heightAnchor)
+        ])
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
 }
