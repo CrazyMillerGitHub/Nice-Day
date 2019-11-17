2c48e6e49fbe546cdd3650a8affc0838ff30cad8
Mikhail Borisov <mishaborisov2013@gmail.com>
2019-11-17 22:12:54 +0300

    -edit SearchCell

--- Nice Day/Presentation layer/SearchViewController/Cell/SearchCell.swift
+++ Nice Day/Presentation layer/SearchViewController/Cell/SearchCell.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class SearchCell: UITableViewCell {
+    
     static var identifier: String = "Cell"
+    
+    enum GradeStatus {
+        case active
+        case passive
+    }
     
     // MARK: textLabel
     let textTitle: UILabel = {
@@ -22,9 +28,9 @@ class SearchCell: UITableViewCell {
     }()
     
     // MARK: DescriptionLabel
-    let dscrTitle: UILabel = {
+    let dscrTitle: (Int) -> UILabel = { xpCount in
         let label = UILabel()
-        let attributedString = NSMutableAttributedString(string: "0,20 xp every minute", attributes: [
+        let attributedString = NSMutableAttributedString(string: "\(xpCount) xp every minute", attributes: [
           .font: UIFont.systemFont(ofSize: 12.0, weight: .semibold),
           .foregroundColor: UIColor.inverseColor,
           .kern: -0.29
@@ -36,7 +42,7 @@ class SearchCell: UITableViewCell {
         label.attributedText = attributedString
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
-    }()
+    }
     
     // MARK: StackView
     let stackView: UIStackView = {
@@ -50,21 +56,42 @@ class SearchCell: UITableViewCell {
         
         return stackView
     }()
-
+    
+    fileprivate var statusGrade: (GradeStatus) -> UIImageView = { imageName in
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.frame.size.height = 16
+        imageView.frame.size.width = 16
+        imageView.image = UIImage(named: imageName == .active ? "activeGrade" : "passiveGrade")
+        return imageView
+    }
+    
+    // MARK: HorizontalStackView
+    let horizontalStackView: UIStackView = {
+        
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .leading
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.backgroundColor = .bgColor
         self.accessoryType = .disclosureIndicator
+        horizontalStackView.addArrangedSubview(statusGrade(.active))
+        horizontalStackView.addArrangedSubview(dscrTitle(Int.random(in: 0...100)))
         stackView.addArrangedSubview(textTitle)
-        stackView.addArrangedSubview(dscrTitle)
+        stackView.addArrangedSubview(horizontalStackView)
         self.addSubview(stackView)
         NSLayoutConstraint.activate([
-            
-            // stackView constrsints
+            // stackView constraints
             stackView.centerYAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerYAnchor),
             stackView.widthAnchor.constraint(equalTo: self.safeAreaLayoutGuide.widthAnchor, multiplier: 0.5),
             stackView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 18)
-
         ])
     }
     
