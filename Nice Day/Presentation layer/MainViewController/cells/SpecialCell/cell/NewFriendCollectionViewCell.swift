56cd26b4b0566bb9013ddea91d90a102ea8caf20
Mikhail Borisov <mishaborisov2013@gmail.com>
2019-12-13 10:40:10 +0300

    -changes

--- Nice Day/Presentation layer/MainViewController/cells/SpecialCell/cell/NewFriendCollectionViewCell.swift
+++ Nice Day/Presentation layer/MainViewController/cells/SpecialCell/cell/NewFriendCollectionViewCell.swift
@@ -10,18 +10,11 @@ import UIKit
 
 class NewFriendCollectionViewCell: UICollectionViewCell {
     
-    // MARK: newFriendImage init
-    private var newFriendImage: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = #imageLiteral(resourceName: "emoji3")
-        imageView.contentMode = .center
-        imageView.translatesAutoresizingMaskIntoConstraints = true
-        return imageView
-    }()
+    static var identifer = "newFriendC"
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
-        prepareUI()
+       
     }
     
     required init?(coder: NSCoder) {
@@ -30,30 +23,6 @@ class NewFriendCollectionViewCell: UICollectionViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        prepareUI()
     }
-    
-    
-    
-}
-private extension NewFriendCollectionViewCell {
-    
-    // MARK: prepareUI (настройка UI)
-    func prepareUI() {
-        refresh()
-        prepareConstraint()
-    }
-    
-    func refresh() {
-        contentView.addSubview(newFriendImage)
         
-    }
-    
-    func prepareConstraint() {
-        NSLayoutConstraint.activate([
-            // newFriendImage Constraint
-            newFriendImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 17),
-            newFriendImage.centerYAnchor.constraint(equalTo: <#T##NSLayoutAnchor<NSLayoutYAxisAnchor>#>, constant: <#T##CGFloat#>)
-        ])
-    }
 }
