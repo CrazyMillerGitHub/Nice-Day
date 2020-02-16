1d35744bbfaac0267de71ab0ab27370968322507
Mikhail Borisov <mishaborisov2013@gmail.com>
2020-02-16 15:15:04 +0300

    added UIView extension

--- Nice Day/Extensions/extension+stackView.swift
+++ Nice Day/Extensions/extension+stackView.swift
@@ -27,3 +27,36 @@ extension UIStackView {
     }
 
 }
+
+extension UIView {
+    
+    func height(constant: CGFloat) {
+        setConstraint(value: constant, attribute: .height)
+    }
+    
+    func width(constant: CGFloat) {
+        setConstraint(value: constant, attribute: .width)
+    }
+    
+    private func removeConstraint(attribute: NSLayoutConstraint.Attribute) {
+        constraints.forEach {
+            if $0.firstAttribute == attribute {
+                removeConstraint($0)
+            }
+        }
+    }
+    
+    private func setConstraint(value: CGFloat, attribute: NSLayoutConstraint.Attribute) {
+        removeConstraint(attribute: attribute)
+        // create constraint
+        let constraint = NSLayoutConstraint(item: self,
+                                            attribute: attribute,
+                                            relatedBy: .equal,
+                                            toItem: nil,
+                                            attribute: .notAnAttribute,
+                                            multiplier: 1.0,
+                                            constant: value)
+        self.addConstraint(constraint)
+    }
+    
+}
